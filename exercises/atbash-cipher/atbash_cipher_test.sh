a3f5a6c0576a17cf70831bde475548f0d26cf1ce
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-07-28 21:42:20 -0400

    made a better template for atbash

--- exercises/atbash-cipher/atbash_cipher_test.sh
+++ exercises/atbash-cipher/atbash_cipher_test.sh
@@ -1,89 +1,90 @@
 #!/usr/bin/env bats
 
 # encode
+
 @test "encode yes" {
   #skip
   run bash atbash_cipher.sh encode "yes"
   [ "$status" -eq 0 ]
-  [ "$output" = "bvh" ]
+  [ "$output" == "bvh" ]
 }
 
 @test "encode no" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "no"
   [ "$status" -eq 0 ]
-  [ "$output" = "ml" ]
+  [ "$output" == "ml" ]
 }
 
 @test "encode OMG" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "OMG"
   [ "$status" -eq 0 ]
-  [ "$output" = "lnt" ]
+  [ "$output" == "lnt" ]
 }
 
 @test "encode spaces" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "O M G"
   [ "$status" -eq 0 ]
-  [ "$output" = "lnt" ]
+  [ "$output" == "lnt" ]
 }
 
 @test "encode mindblowingly" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "mindblowingly"
   [ "$status" -eq 0 ]
-  [ "$output" = "nrmwy oldrm tob" ]
+  [ "$output" == "nrmwy oldrm tob" ]
 }
 
 @test "encode numbers" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "Testing,1 2 3, testing."
   [ "$status" -eq 0 ]
-  [ "$output" = "gvhgr mt123 gvhgr mt" ]
+  [ "$output" == "gvhgr mt123 gvhgr mt" ]
 }
 
 @test "encode deep thought" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "Truth is fiction."
   [ "$status" -eq 0 ]
-  [ "$output" = "gifgs rhurx grlm" ]
+  [ "$output" == "gifgs rhurx grlm" ]
 }
 
 @test "encode all the letters" {
-  skip
+  #skip
   run bash atbash_cipher.sh encode "The quick brown fox jumps over the lazy dog."
   [ "$status" -eq 0 ]
-  [ "$output" = "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt" ]
+  [ "$output" == "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt" ]
 }
 
 # decode
+
 @test "decode exercism" {
-  skip
+  #skip
   run bash atbash_cipher.sh decode "vcvix rhn"
   [ "$status" -eq 0 ]
-  [ "$output" = "exercism" ]
+  [ "$output" == "exercism" ]
 }
 
 @test "decode a sentence" {
-  skip
+  #skip
   run bash atbash_cipher.sh decode "zmlyh gzxov rhlug vmzhg vkkrm thglm v"
   [ "$status" -eq 0 ]
-  [ "$output" = "anobstacleisoftenasteppingstone" ]
+  [ "$output" == "anobstacleisoftenasteppingstone" ]
 }
 
 @test "decode numbers" {
-  skip
+  #skip
   run bash atbash_cipher.sh decode "gvhgr mt123 gvhgr mt"
   [ "$status" -eq 0 ]
-  [ "$output" = "testing123testing" ]
+  [ "$output" == "testing123testing" ]
 }
 
 @test "decode all the letters" {
-  skip
+  #skip
   run bash atbash_cipher.sh decode "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
   [ "$status" -eq 0 ]
-  [ "$output" = "thequickbrownfoxjumpsoverthelazydog" ]
+  [ "$output" == "thequickbrownfoxjumpsoverthelazydog" ]
 }
-
 
