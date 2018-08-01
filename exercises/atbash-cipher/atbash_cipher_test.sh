56c6283279ef61a12096e561f057b4ce6129c5bd
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-01 11:24:04 -0400

    updated atbash-cipher

--- exercises/atbash-cipher/atbash_cipher_test.sh
+++ exercises/atbash-cipher/atbash_cipher_test.sh
@@ -10,49 +10,49 @@
 }
 
 @test "encode no" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "no"
   [ "$status" -eq 0 ]
   [ "$output" == "ml" ]
 }
 
 @test "encode OMG" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "OMG"
   [ "$status" -eq 0 ]
   [ "$output" == "lnt" ]
 }
 
 @test "encode spaces" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "O M G"
   [ "$status" -eq 0 ]
   [ "$output" == "lnt" ]
 }
 
 @test "encode mindblowingly" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "mindblowingly"
   [ "$status" -eq 0 ]
   [ "$output" == "nrmwy oldrm tob" ]
 }
 
 @test "encode numbers" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "Testing,1 2 3, testing."
   [ "$status" -eq 0 ]
   [ "$output" == "gvhgr mt123 gvhgr mt" ]
 }
 
 @test "encode deep thought" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "Truth is fiction."
   [ "$status" -eq 0 ]
   [ "$output" == "gifgs rhurx grlm" ]
 }
 
 @test "encode all the letters" {
-  #skip
+  skip
   run bash atbash_cipher.sh encode "The quick brown fox jumps over the lazy dog."
   [ "$status" -eq 0 ]
   [ "$output" == "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt" ]
@@ -61,30 +61,44 @@
 # decode
 
 @test "decode exercism" {
-  #skip
+  skip
   run bash atbash_cipher.sh decode "vcvix rhn"
   [ "$status" -eq 0 ]
   [ "$output" == "exercism" ]
 }
 
 @test "decode a sentence" {
-  #skip
+  skip
   run bash atbash_cipher.sh decode "zmlyh gzxov rhlug vmzhg vkkrm thglm v"
   [ "$status" -eq 0 ]
   [ "$output" == "anobstacleisoftenasteppingstone" ]
 }
 
 @test "decode numbers" {
-  #skip
+  skip
   run bash atbash_cipher.sh decode "gvhgr mt123 gvhgr mt"
   [ "$status" -eq 0 ]
   [ "$output" == "testing123testing" ]
 }
 
 @test "decode all the letters" {
-  #skip
+  skip
   run bash atbash_cipher.sh decode "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
   [ "$status" -eq 0 ]
   [ "$output" == "thequickbrownfoxjumpsoverthelazydog" ]
+}
+
+@test "decode with too many spaces" {
+  skip
+  run bash atbash_cipher.sh decode "vc vix    r hn"
+  [ "$status" -eq 0 ]
+  [ "$output" == "exercism" ]
+}
+
+@test "decode with no spaces" {
+  skip
+  run bash atbash_cipher.sh decode "zmlyhgzxovrhlugvmzhgvkkrmthglmv"
+  [ "$status" -eq 0 ]
+  [ "$output" == "anobstacleisoftenasteppingstone" ]
 }
 
