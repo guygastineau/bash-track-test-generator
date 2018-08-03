61bdef9860c45e087ad2be4042d0b755cc0d72b2
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-03 10:30:58 -0400

    fixed typo in old test names

---
+++ exercises/word-count/word_count.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+input="$1"
+
+if [[ ! -n "${input}" ]]
+then
+	echo "Usage: $0 <input>"
+	exit
+fi
+
+echo "$input" \
+  | tr '[:upper:]' '[:lower:]' \
+  | tr '.,:&@$%^!' ' ' \
+  | sed "s/^\([\"']\)\(.*\)\1\$/\2/g" \
+  | awk -f <(cat - <<-'HEREDOC'
+      {
+        for (field_index = 1; field_index <= NF; field_index++)
+        {
+          result[$field_index]++
+        }
+      }
+      END {
+        for (field_index in result)
+        {
+          printf "%s: %d\n", field_index, result[field_index]
+        }
+      }
+HEREDOC
+)
+
+exit $?
