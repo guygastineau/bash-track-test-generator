cd3097393b1339c8937df63817e085da61e08ec7
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-08 19:44:32 -0400

    added collatz-conjecture

---
+++ exercises/collatz-conjecture/collatz_conjecture.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$1" -gt 0 ]; then
+    N=$1
+else
+    echo "Error: Only positive numbers are allowed"
+    exit 1
+fi
+
+STEP=0
+
+while [ "$N" -ne "1" ]
+do
+    if [ "$(($N % 2))" -eq "0" ]; then
+        N=$(($N / 2))
+    else
+        N=$((($N * 3) + 1))
+    fi
+    STEP=$(($STEP + 1))
+done
+
+echo $STEP
