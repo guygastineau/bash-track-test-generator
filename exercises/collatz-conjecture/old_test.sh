092be61fcd684e69e32c4d09cbb400bb32481ef3
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-09 12:39:44 -0400

    updated collatz conjecture

--- exercises/collatz-conjecture/old_test.sh
+++ exercises/collatz-conjecture/old_test.sh
@@ -8,37 +8,37 @@
 }
 
 @test "divide if even" {
-  #skip
+  skip
   run bash collatz_conjecture.sh 16
   [ "$status" -eq 0 ]
   [ "$output" -eq 4 ]
 }
 
 @test "even and odd steps" {
-  #skip
+  skip
   run bash collatz_conjecture.sh 12
   [ "$status" -eq 0 ]
   [ "$output" -eq 9 ]
 }
 
 @test "Large number of even and odd steps" {
-  #skip
+  skip
   run bash collatz_conjecture.sh 1000000
   [ "$status" -eq 0 ]
   [ "$output" -eq 152 ]
 }
 
 @test "zero is an error" {
-  #skip
+  skip
   run bash collatz_conjecture.sh 0
-  [ "$status" -eq 0 ]
+  [ "$status" -eq 1 ]
   [ "$output" == "Error: Only positive numbers are allowed" ]
 }
 
 @test "negative value is an error" {
-  #skip
+  skip
   run bash collatz_conjecture.sh -15
-  [ "$status" -eq 0 ]
+  [ "$status" -eq 1 ]
   [ "$output" == "Error: Only positive numbers are allowed" ]
 }
 
